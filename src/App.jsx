ba5fdfa753056d48ad92f0ea1f6bc329c3d698a3
Eddglitch <eduardtelv@gmail.com>
2023-06-01 19:11:45 -0600

    Commit inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+
+
+function WebContent() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="bg-circle1"> </div>
+      <div className="bg-circle2"> </div>
+
+      <Routes>
+        
+      </Routes>
+
+    </>
   );
 }
 
-export default App;
+export default WebContent;
